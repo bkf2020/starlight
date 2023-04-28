045fba1b884a1f61b32ee36dbcbfeb3bf36109eb
bkf2020 <74793713+bkf2020@users.noreply.github.com>
2023-04-27 23:21:18 -0700

    Create new InsightCluster model

--- problems/admin.py
+++ problems/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Hint, Insight, HintCluster
+from .models import Hint, Insight, HintCluster, InsightCluster
 
 # Register your models here.
 admin.site.register(Hint)
 admin.site.register(Insight)
 admin.site.register(HintCluster)
+admin.site.register(InsightCluster)
--- problems/models.py
+++ problems/models.py
@@ -22,11 +22,25 @@ class HintCluster(models.Model):
     cluster_id = models.IntegerField(default=-1)
     first = models.BooleanField(default=False)
 
+class InsightCluster(models.Model):
+    problem_id = models.IntegerField(default=-1)
+    insight = models.ForeignKey(Insight, null=True, on_delete=models.CASCADE)
+    cluster_id = models.IntegerField(default=-1)
+    first = models.BooleanField(default=False)
+
 def update_first_hint_cluster(sender, **kwargs):
     new_first = HintCluster.objects.first()
     new_first.first = True
     new_first.save()
 
+def update_first_insight_cluster(sender, **kwargs):
+    new_first = InsightCluster.objects.first()
+    new_first.first = True
+    new_first.save()
+
 post_delete.connect(
     update_first_hint_cluster, sender=Hint
+)
+post_delete.connect(
+    update_first_insight_cluster, sender=Insight
 )
