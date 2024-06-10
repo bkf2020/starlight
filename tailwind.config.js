0364cc5884f6b89eb6f42eb01db4426be0b6c3eb
bkf2020 <74793713+bkf2020@users.noreply.github.com>
2024-06-09 23:18:23 -0700

    Change colorscheme of light/dark mode, adjust login button placement

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,40 @@ module.exports = {
     'modal',
     'hidden',
   ],
-  darkMode: 'selector',
-  plugins: [require('daisyui')]
+  darkMode: ['class', '[data-theme="dark"]'],
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+      light: {
+        ...require("daisyui/src/theming/themes")["light"],
+	primary: "#1d4ed8",
+	secondary: "#0369a1",
+        accent: "#047857",
+	info: "#0e7490",
+	warning: "#eab308",
+	success: "#15803d",
+	error: "#be123c"
+      },
+      dark: {
+        ...require("daisyui/src/theming/themes")["dark"],
+	primary: "#60a5fa",
+	secondary: "#38bdf8",
+        accent: "#34d399",
+	info: "#06b6d4",
+	warning: "#facc15",
+	success: "#22c55e",
+	error: "#fb7185",
+	"base-100": "#171717",
+	"base-200": "#262626",
+	"base-300": "#404040",
+	"color": "#d2d2d2",
+	"neutral": "#404040",
+	"base-content": "#d2d2d2"
+      },
+      }
+    ],
+     darkTheme: 'light'
+  },
 }
 
